c2ca95d49f17a53d6d8a9060468fe7bce7615d12
Maruf512 <marufsarkar512@gmail.com>
2024-09-16 20:52:57 +0600

    Done Hero Section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Image1 from "../../assets/hero/women.png"
 import Image2 from "../../assets/hero/shopping.png"
 import Image3 from "../../assets/hero/sale.png"
+import Slider from "react-slick";
 
 const ImageList = [
     {
@@ -26,6 +27,20 @@ const ImageList = [
 
 
 function Hero() {
+
+    var settings = {
+        dots: false,
+        arrows: false,
+        infinite: true,
+        speed: 800,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        cssEase: "ease-in-out",
+        pauseOnHover: false,
+        pauseOnFocus: true,
+    };
+
   return (
     <div className='relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200'>
         {/* background pattern */}
@@ -34,28 +49,33 @@ function Hero() {
         </div>
         {/* hero section */}
         <div className='container pb-8 sm:pb-0'>
-            <div>
-                <div className='grid grid-cols-1 sm:grid-cols-2'>
-                    {/* text content Section */}
+            <Slider {...settings}>
+                {ImageList.map((data) =>(
                     <div>
-                        <h1 className='text-5xl sm:text-6xl lg:text-7xl font-bold'>Lorem ipsum dolor sit.</h1>
-                        <p className='text-sm'>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, voluptate.
-                        </p>
-                        <div>
-                            <button className='bg-gradient-to-r from-primary hover:scale-105 duration-200 text-white py-2 px-4 rounded-full'>Order Now</button>
+                    <div className='grid grid-cols-1 sm:grid-cols-2'>
+                        {/* text content Section */}
+                        <div className='flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10'>
+                            <h1 className='text-5xl sm:text-6xl lg:text-7xl font-bold'>{data.title}</h1>
+                            <p className='text-sm'>
+                                {data.description}
+                            </p>
+                            <div>
+                                <button className='bg-gradient-to-r from-primary hover:scale-105 duration-200 text-white py-2 px-4 rounded-full'>Order Now</button>
+                            </div>
                         </div>
-                    </div>
-                    {/* Image Section */}
-                    <div>
-                        <div>
-                            <img src={Image1} alt=""
-                            className='w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-125 object-contain mx-auto'
-                            />
+                        {/* Image Section */}
+                        <div className='order-1 sm:order-2'>
+                            <div className='relative z-10'>
+                                <img src={data.img} alt=""
+                                className='w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-125 object-contain lg:scale-120 mx-auto'
+                                />
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+                ))}
+            </Slider>
+            
         </div>
     </div>
   )
